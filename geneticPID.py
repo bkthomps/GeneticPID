0ba61dbedf5c76b6c747fe568ffe278db12021e1
bkthomps <bkthomps@edu.uwaterloo.ca>
2020-07-19 20:40:13 -0400

    Add representation information

--- geneticPID.py
+++ geneticPID.py
@@ -1,7 +1,6 @@
 from random import random
 import matplotlib.pyplot as plot
-
-from control import TransferFunction, feedback, step_response, series
+from control import TransferFunction, feedback, step_response, series, step_info
 
 F = TransferFunction(1, [1, 6, 11, 6, 0])
 T = []
@@ -14,13 +13,18 @@ while count < 100:
 def compute_ise(Kp, Ti, Td):
     g = Kp * TransferFunction([Ti * Td, Ti, 1], [Ti, 0])
     sys = feedback(series(g, F), 1)
+    sys_info = step_info(sys)
     _, y = step_response(sys, T)
-    return sum((y - 1) ** 2)
+    ise = sum((y - 1) ** 2)
+    t_r = sys_info['RiseTime']
+    t_s = sys_info['SettlingTime']
+    m_p = sys_info['Overshoot']
+    return ise, t_r, t_s, m_p
 
 
 def genetic_algorithm(generation_count, population_size, crossover_prob, mutate_prob):
     parents = [[8.0, 4.0, 0.5], [12.0, 6.0, 1.5]]
-    best_factor = 0.0
+    best_factor, best_tr, best_ts, best_mp = 0.0, 0.0, 0.0, 0.0
     best_factor_list = []
     i = 0
     while i < generation_count:
@@ -39,11 +43,18 @@ def genetic_algorithm(generation_count, population_size, crossover_prob, mutate_
         factor_total = 0.0
         factor_list = []
         for child in children:
-            ise = compute_ise(child[0], child[1], child[2])
-            factor = 1 / ise
+            try:
+                ise, t_r, t_s, m_p = compute_ise(child[0], child[1], child[2])
+                factor = 1 / ise
+            except IndexError:
+                factor, t_r, t_s, m_p = 0.0, 0.0, 0.0, 0.0
             factor_list.append(factor)
             factor_total += factor
-            best_factor = max(best_factor, factor)
+            if factor > best_factor:
+                best_factor = factor
+                best_tr = t_r
+                best_ts = t_s
+                best_mp = m_p
         k = 0
         while len(parents) < 2:
             if factor_list[k] > random() * factor_total:
@@ -52,6 +63,7 @@ def genetic_algorithm(generation_count, population_size, crossover_prob, mutate_
         print(i, best_factor)
         best_factor_list.append(best_factor)
         i += 1
+    print("Best: t_r = ", best_tr, ", t_s = ", best_ts, ", m_p", best_mp)
     plot.plot(best_factor_list)
     plot.ylabel("Fitness")
     plot.xlabel("Generation")
